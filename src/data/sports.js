2de85624ceb59de1d1ed9bbadf94a620cb520e33
Acampbell <78562675+fu4303@users.noreply.github.com>
2021-07-15 09:11:07 -0400

    Create sports.js

---
+++ src/data/sports.js
@@ -0,0 +1,41 @@
+const sports = [
+  {
+    title: 'The Getting Simple',
+    description:
+      'How to be more productive and creative while enjoying a slower, more‑balanced life.',
+    image: 'http://tiny.cc/1zxiaz',
+    url: 'https://gettingsimple.com/podcast',
+  },
+  {
+    title: 'The Tim Ferriss Show',
+    description: 'A podcast about tactics, tools, and routines you can use.',
+    image: 'http://tiny.cc/hcyiaz',
+    url: 'https://tim.blog/podcast/',
+  },
+  {
+    title: 'Eat Sleep Work Repeat',
+    description: 'A podcast about making work better.',
+    image: 'http://tiny.cc/y2xiaz',
+    url: 'https://eatsleepworkrepeat.fm/podcast/',
+  },
+  {
+    title: 'The Minimalists',
+    description: 'A podcast about living a meaningful life with less',
+    image: 'http://tiny.cc/u4xiaz',
+    url: 'https://www.theminimalists.com/podcast/',
+  },
+  {
+    title: 'Kwik Brain',
+    description: 'A podcast about memory improvement, brain performance, and accelerated learning.',
+    image: 'http://tiny.cc/o6xiaz',
+    url: 'https://jimkwik.com/category/podcast/',
+  },
+  {
+    title: 'The PMP',
+    description: 'A podcast about about productivity, business and self-improvement.',
+    image: 'http://tiny.cc/17xiaz',
+    url: 'https://paulminors.com/podcast/',
+  },
+];
+
+export default sports;
